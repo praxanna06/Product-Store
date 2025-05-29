2b98f1837cf4528ba40e133738687b483ba003aa
Prasanna <mprasannaragav@gmail.com>
2025-05-29 11:08:55 +0400

    Added Customer Support

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 import CreatePage from "./pages/CreatePage";
 import HomePage from "./pages/HomePage";
 import Navbar from "./component/Navbar";
+import { BubbleChat } from 'flowise-embed-react'
 
 function App() {
 
   return (
     <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+      <BubbleChat
+            chatflowid="60c4f029-c394-4a23-84e9-400579950580"
+            apiHost="https://flowiseai-p5t6.onrender.com"
+        />
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
