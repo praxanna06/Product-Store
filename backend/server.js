a79420d8cfd87864c9e1fee74c5d0cca8290c81c
Prasanna <mprasannaragav@gmail.com>
2025-06-05 23:36:01 +0400

    Fixed

--- backend/server.js
+++ backend/server.js
@@ -23,12 +23,12 @@ app.use(express.json());
 
 app.use("/api/products",productRoute);
 
-// if (process.env.NODE_ENV === "production") {
-// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-// 	app.get(/(.*)/, (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-// 	});
-// }
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get(/(.*)/, (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
     connectDB();
