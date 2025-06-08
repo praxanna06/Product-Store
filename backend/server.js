5c36015cf42947ad6bf920d75bb790d5482bf811
Prasanna <mprasannaragav@gmail.com>
2025-06-08 12:43:01 +0400

    Fixed

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,19 @@
 import express from 'express';
-import dotenv from "dotenv";
 import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRoute from "./routes/productr.js";
-//const cors = require('cors');
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load env vars from .env inside /backend
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
-
-//app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // or "*" for any origin
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -21,17 +22,16 @@ app.use(function(req, res, next) {
 
 app.use(express.json());
 
-app.use("/api/products",productRoute);
+app.use("/api/products", productRoute);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-	app.get(/(.*)/, (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+  });
 }
 
 app.listen(PORT, () => {
-    connectDB();
-    console.log("Server started at http://localhost:"+ PORT);
-})
-
+  connectDB();
+  console.log("Server started at http://localhost:" + PORT);
+});
