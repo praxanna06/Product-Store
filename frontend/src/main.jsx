2af24969868553d3c3222190c6ca22ea1edbb1b9
Prasanna <mprasannaragav@gmail.com>
2025-06-08 12:29:46 +0400

    Bug Fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom"
 import { ChakraProvider } from '@chakra-ui/react'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
